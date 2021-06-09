0828eb76bd74922623f5cf2e3c50b7d82da55516
Cloud User <centos@ip-172-31-6-147.eu-west-2.compute.internal>
2021-06-09 11:25:10 +0000

    ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,41 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+
+`timescale 1ns / 100ps
+
+module monitor (
+    //Todo: add ports 
+	input clk,
+	input rst,
+	input button,
+	
+
+	output[2:0]colour
+    );
+	reg[2:0]colour;
+
+
+	always @ (posedge clk or posedge rst) begin
+		if (rst) begin
+			colour = 1;
+		end
+		else if ((button == 1) && (colour == 6)) begin
+			colour <= 1;
+		end
+		else if (button ==1) begin
+			colour <= colour + 1;
+		end
+	end
+
+
+
+
+
+
+
+endmodule
+
+
+
+
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -6,4 +6,84 @@
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+);
+
+parameter CLK_PERIOD = 10;
+
+
+reg clk;
+reg rst;
+reg button;
+wire [2:0]colour;
+reg [2:0]colour_prev;
+reg err;
+
+
+ initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+initial begin
+ err = 0;
+ clk = 0;
+ rst = 1;
+ button = 0;
+ colour_prev = 1;
+
+#CLK_PERIOD
+
+if (rst) begin
+ if (colour != 1) begin
+    $display("***TEST FAILED! did not reset correct ***");
+           err=1;
+end
+end
+
+rst = 0;
+ forever begin
+  #CLK_PERIOD
+
+if (button == 1) begin
+if ((colour != colour_prev+1) && (colour_prev != 6) && (colour != 1)) begin
+$display("***TEST FAILED! did not shift correct ***");
+           err=1;
+end
+end
+
+button =~ button;
+colour_prev = colour;
+
+
+end
+end
+
+initial begin 
+ #200
+ if (err == 0)begin
+  $display("***TEST PASSED! :) ***");
+  end
+ $finish;
+end
+
+monitor top (
+.clk (clk),
+.rst (rst),
+.button (button),
+.colour (colour)
+);
+endmodule 
+
+
+
+
+
+ 
+
+
+ 
