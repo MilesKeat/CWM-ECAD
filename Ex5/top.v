17494514cad95b790aef6f319b936b463399a1e4
Cloud User <centos@ip-172-31-6-147.eu-west-2.compute.internal>
2021-06-09 15:59:08 +0000

    ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -13,3 +13,59 @@
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
 
+
+`timescale 1ns / 100ps
+
+module monitor (
+    //Todo: add ports 
+	input clk,
+	input [4:0]temperature,
+	input sett,
+
+	output heating,
+	output cooling
+
+    );
+reg heating;
+reg cooling;
+reg [1:0]state;
+
+always @ (posedge clk) begin
+if (sett !=1) begin
+heating = 0;
+cooling = 0;
+end 
+
+state = (heating<<1+cooling);
+if (state == 2'b10) begin
+if (temperature >= 5'b10100) begin
+heating = 0;
+end 
+end
+
+else if (state == 2'b01) begin
+if (temperature <= 5'b10100 ) begin
+cooling = 0 ;
+end
+end 
+else if (state == 2'b00) begin
+if (temperature <= 5'b10010) begin
+heating = 1;
+end
+if (temperature >= 5'b10110) begin
+cooling = 1;
+end 
+end 
+else if (state == 2'b11) begin
+cooling = 0;
+heating = 0;
+end 
+end
+endmodule 
+
+
+
+
+
+
+
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -6,4 +6,88 @@
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+    );
+ parameter CLK_PERIOD = 10;
+ reg clk;
+ reg [4:0] temperature;
+ reg [1:0]state;
+ reg [1:0]state_prev;
+ reg sett;
+wire heating;
+wire cooling;
+reg err;
+
+  initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+initial begin
+err = 0;
+clk = 0;
+state_prev = 00;
+temperature = 16;
+state = 00;
+sett = 1;
+#CLK_PERIOD
+sett = 0;
+forever begin
+ state_prev = (heating<<1+cooling);
+ #CLK_PERIOD
+ state = (heating<<1+cooling);
+ if ((temperature >= 22) && (state_prev == 2'b00)) begin 
+ if (state != 2'b01)begin
+ $display("***TEST FAILED! did not switch correct ***");
+           err=1;
+ end 
+ end 
+ if ((temperature >= 20) && (state_prev == 2'b10)) begin 
+ if (state != 2'b00)begin
+ $display("***TEST FAILED! did not switch correct ***");
+           err=1;
+ end
+ end 
+ if ((temperature <= 18) && (state_prev == 2'b00)) begin 
+ if (state != 2'b10)begin
+ $display("***TEST FAILED! did not switch correct ***");
+           err=1;
+ end
+ end 
+ if ((temperature <=20) && (state_prev == 2'b01)) begin 
+ if (state != 2'b00)begin
+ $display("***TEST FAILED! did not switch correct ***");
+           err=1;
+ end
+ end 
+ 
+
+ if (temperature < 23) begin
+ temperature = temperature + 3;
+ end
+ else begin
+ temperature = temperature - 6;
+ end
+ end 
+ end
+
+initial begin 
+ #200
+ if (err == 0)begin
+  $display("***TEST PASSED! :) ***");
+  end
+ $finish;
+end
+
+monitor top (
+.clk (clk),
+.temperature (temperature),
+.heating (heating),
+.cooling (cooling),
+.sett (sett)
+);
+endmodule 
