9aaa95385b21c652a91b6ba50c8b7d7a86d45389
Cloud User <centos@ip-172-31-6-147.eu-west-2.compute.internal>
2021-06-11 10:51:19 +0000

    ex7

--- Ex7/top.v
+++ Ex7/top.v
@@ -14,4 +14,88 @@
 //           light [23:0]
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
 
+module three_system (
+input clk, sel, rst, button, enable,
+output light
+);
+reg [23:0]light;
+reg [2:0]colourtemp;
+reg [23:0]rgbtemp;
+
+always begin
+lights stage1(clk,rst,button,colourtemp);
+converter stage2(clk, colourtemp, enable, rgbtemp);
+doorbell stage3(white, rgbtemp, sel, light);
+end
+
+endmodule
+
+module doorbell(
+    //Todo: define inputs here
+    input [23:0]white,
+    input [23:0]rgb,
+    input sel,
+    output out
+    );
+
+    //Todo: define registers and wires here
+    reg out;
+	
+    //Todo: define your logic here
+   always begin 
+    if(sel)
+     out <= rgb;
+    else
+     out <= white;
+    end
+                      
+      
+endmodule
+
+module lights (
+    //Todo: add ports 
+	input clk,
+	input rst,
+	input button,
+	
+	output[2:0]colour
+    );
+	reg[2:0]colour;
+
+
+	always @ (posedge clk or posedge rst) begin
+		if (rst) begin
+			colour = 1;
+		end
+		else if ((button == 1) && (colour == 6)) begin
+			colour <= 1;
+		end
+		else if (button ==1) begin
+			colour <= colour + 1;
+		end
+	end
+endmodule
+
+module converter (
+input clk,
+input [2:0]colour,
+input enable,
+
+
+
+output [23:0]rgb 
+);
+
+
+blk_mem_gen_0 your_instance_name (
+  .clka(clk),    // input wire clka
+  .ena(enable),      // input wire ena
+  .wea(0),      // input wire [0 : 0] wea
+  .addra(colour),  // input wire [2 : 0] addra
+  .dina(24'b0),    // input wire [23 : 0] dina
+  .douta(rgb)  // output wire [23 : 0] douta
+);
+
+endmodule
--- Ex7/top_tb.v
+++ Ex7/top_tb.v
@@ -6,4 +6,65 @@
 // Description: A testbench module to test Ex7 - Lights Selector
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+module top_tb(
+    );
+parameter CLK_PERIOD = 10;
+reg clk;
+reg sel;
+reg rst;
+reg enable;
+reg button;
+wire [23:0]light;
+reg err = 0;
+
+  initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+initial begin 
+clk = 0;
+err = 0;
+enable = 0;
+rst = 1;
+sel = 1;
+
+#CLK_PERIOD
+#CLK_PERIOD
+#CLK_PERIOD
+#CLK_PERIOD
+
+if (rst) begin
+ if (light != 24'b0) begin
+    $display("***TEST FAILED! did not reset correct ***");
+           err=1;
+end
+end
+sel = 0;
+#CLK_PERIOD
+#CLK_PERIOD
+#CLK_PERIOD
+#CLK_PERIOD
+
+if (sel) begin
+ if (light != 6'hFFFFFF) begin
+  $display("***TEST FAILED! sel does not work ***");
+           err=1;
+end
+end
+end
+three_system top ( 
+.clk (clk),
+.sel (sel),
+.rst (rst),
+.button (button),
+.light (light),
+.enable (enable)
+);
+
+endmodule
+
 
